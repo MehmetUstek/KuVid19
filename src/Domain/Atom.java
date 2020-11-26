e2d96aefb493b646974a60aa8edd56bb794fbf4a
Mehmet <mustek17@ku.edu.tr>
2020-11-26 20:44:01 +0300

    Initial commit

--- src/Domain/Atom.java
+++ src/Domain/Atom.java
@@ -1,5 +1,26 @@
 package Domain;
 
 public abstract class Atom {
-
+	String atomType;
+	double movementAngle;
+	int x,y;
+	double speed;
+	double diameter;
+	
+	public Atom(double movementAngle, double speed, double diameter,String atomType) {
+		this.movementAngle= movementAngle;
+		this.speed= speed;
+		this.diameter= diameter;
+		this.atomType= atomType;
+	}
+	public void move() {
+		
+	}
+	public void shoot(double angle) {
+		
+	}
+	public void bounceFromWall() {
+		
+	}
+	
 }
